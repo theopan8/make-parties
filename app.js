a4c83c28098a9375ba87a0f216aa4d44c8793ba9
theopan8 <tpan729@gmail.com>
2021-09-09 16:43:52 -0700

    init

---
+++ app.js
@@ -0,0 +1,26 @@
+// Initialize express
+const express = require('express');
+const methodOverride = require('method-override');
+const app = express();
+// require handlebars
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const models = require('./db/models');
+
+// Use "main" as our default layout
+app.engine('handlebars', exphbs({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+// Use handlebars to render
+app.set('view engine', 'handlebars');
+app.use(express.urlencoded());
+app.use(methodOverride('_method'));
+
+require('./controllers/events')(app, models);
+
+// Choose a port to listen on
+const port = process.env.PORT || 3000;
+
+// Tell the app what port to listen on
+app.listen(port, () => {
+    console.log('App listening on port 3000!')
+});
