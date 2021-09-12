f28a52daff101cca984a0cb4290dfc7f05b3fe65
theopan8 <tpan729@gmail.com>
2021-09-11 17:16:31 -0700

    Users can delete rsvps

--- controllers/rsvps.js
+++ controllers/rsvps.js
@@ -17,5 +17,13 @@ module.exports = (app, models) => {
             console.log(err)
         });
     });
-    // DESTROY  
+    // DESTROY
+    app.delete('/events/:eventId/rsvps/:id', (req, res) => {
+        models.Rsvp.findByPk(req.params.id).then(rsvp => {
+            rsvp.destroy();
+            res.redirect(`/events/${req.params.eventId}`);
+        }).catch((err) => {
+            console.log(err);
+        });
+    });
 }
